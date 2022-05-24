26eb0832863af9142aadd637a10d5276b451331c
El Jahrani Aymane <aymane.el-jahrani@etu.univ-orleans.fr>
2022-05-24 12:54:09 +0200

    Switched to net48 to have a linear string.getHashCode()

--- SameHashCodeTest/UnitTest.cs
+++ SameHashCodeTest/UnitTest.cs
@@ -3,19 +3,19 @@ namespace SameHashCodeTest
     [TestClass]
     public class UnitTest
     {
-        [TestMethod]
-        public async Task get_random_string_with_given_length_should_return_string_with_same_length()
-        {
-            var length = 100000;
-            var result = await RandomStringGenerator.GetRandomString(length);
-            Assert.AreEqual(length, result.Length);
-        }
+        //[TestMethod]
+        //public async Task get_random_string_with_given_length_should_return_string_with_same_length()
+        //{
+        //    var length = 100000;
+        //    var result = await RandomStringGenerator.GetRandomString(length);
+        //    Assert.AreEqual(length, result.Length);
+        //}
 
-        [TestMethod]
-        public void get_same_hash_string_should_return_different_string_with_same_hash()
-        {
-            var s = "IO";
-            Assert.IsTrue(Program.GetRandomString(s.Length).Result.GetHashCode().Equals(Program.GetSameHashDifferentString(s).Result));
-        }
+        //[TestMethod]
+        //public void get_same_hash_string_should_return_different_string_with_same_hash()
+        //{
+        //    var s = "IO";
+        //    Assert.IsTrue(Program.GetRandomString(s.Length).Result.GetHashCode().Equals(Program.GetSameHashDifferentString(s).Result));
+        //}
     }
 }
--- optim-SameHashCode/Program.cs
+++ optim-SameHashCode/Program.cs
@@ -8,19 +8,34 @@ namespace sameHashCodeStrings
 
     public static class Program
     {
-        async static Task Main()
+         static void Main(string[] args)
         {
+            string a = "AB";
+            Console.WriteLine("{0} {1} {2}", a.GetHashCode(),"B".GetHashCode(),"C".GetHashCode());
+            Console.ReadLine();
 
-            /* String Hashcode Formula : s[0]*31^(n-1) + s[1]*31^(n-2) + ... + s[n-1] // where s[n] == ASCII code 
-            
-            ReverseEngineering + maths to guess?
-            
-            //Get a first random string
+            /* String Hashcode Formula in C# :
+             * 
+             * fixed (char* ptr = this)
+            {
+                int num = 352654597;
+                int num2 = num;
+                int* ptr2 = (int*)ptr;
+                int num3;
+                for (num3 = Length; num3 > 2; num3 -= 4)
+                {
+                    num = ((num << 5) + num + (num >> 27)) ^ *ptr2;
+                    num2 = ((num2 << 5) + num2 + (num2 >> 27)) ^ ptr2[1];
+                    ptr2 += 2;
+                }
 
-            //Guess a second string that has same hash but !Equal
+                if (num3 > 0)
+                {
+                    num = ((num << 5) + num + (num >> 27)) ^ *ptr2;
+                }
 
-            //Guess a third string that has same hash but !Equal
-
+                return num + num2 * 1566083941;
+            }
             */
 
 
@@ -33,34 +48,34 @@ namespace sameHashCodeStrings
             return hashCodeEqual && stringNotEqual;
         }
 
-        public async static Task<string> GetRandomString(int length)
-        {
-            throw new NotImplementedException();
-        }
-        public async static Task<int> GetSameHashDifferentString(string s)
-        {
-            throw new NotImplementedException();
-        }
+        //public async static Task<string> GetRandomString(int length)
+        //{
+        //    throw new NotImplementedException();
+        //}
+        //public async static Task<int> GetSameHashDifferentString(string s)
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 
     public class RandomStringGenerator 
     {
         internal static readonly char[] _chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
 
-        public static async Task<string> GetRandomString(int length = 2) 
-        {
-            char[] result = new char[length];
-            var partitioner = Partitioner.Create(0,length);
-            Parallel.ForEach(partitioner, (range, loopstate) =>
-            {
-                for (int i = range.Item1; i < range.Item2; i++)
-                {
-                    result[i] = _chars[RandomNumberGenerator.GetInt32(_chars.Length - 1)];
-                }
-            });
-            return new String(result);
+        ////public static async Task<string> GetRandomString(int length = 2) 
+        ////{
+        ////    char[] result = new char[length];
+        ////    var partitioner = Partitioner.Create(0,length);
+        ////    Parallel.ForEach(partitioner, (range, loopstate) =>
+        ////    {
+        ////        for (int i = range.Item1; i < range.Item2; i++)
+        ////        {
+        ////            result[i] = _chars[RandomNumberGenerator.GetInt32(_chars.Length - 1)];
+        ////        }
+        ////    });
+        ////    return new String(result);
            
-        }
+        ////}
 
      
     }
